e15444129b6864af744634771c1d9feb500c1aba
liuzhao <liuzhao@woncan.com>
2024-12-20 14:37:49 +0800

    修改分离提醒监听

--- app/src/main/java/com/woncan/devicegithubsdk/MainActivity.java
+++ app/src/main/java/com/woncan/devicegithubsdk/MainActivity.java
@@ -4,46 +4,35 @@ import static java.lang.Thread.sleep;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
-import android.view.View;
 import android.widget.Toast;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.woncan.device.Device;
-import com.woncan.device.Jni;
-import com.woncan.device.JniImp;
 import com.woncan.device.NMEA;
 import com.woncan.device.ScanManager;
 import com.woncan.device.bean.DeviceInfo;
 import com.woncan.device.bean.DeviceNtripAccount;
+import com.woncan.device.bean.SatelliteInfo;
 import com.woncan.device.bean.WLocation;
 import com.woncan.device.device.DeviceInterval;
-import com.woncan.device.listener.AttachmentStateListener;
 import com.woncan.device.listener.DeviceStatesListener;
+import com.woncan.device.listener.SatelliteListener;
 import com.woncan.device.listener.WLocationListener;
-import com.woncan.device.uitl.AttachmentState;
 import com.woncan.devicegithubsdk.databinding.ActivityMainBinding;
 
-import java.io.IOException;
-import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-
-import kotlin.Unit;
-import kotlin.jvm.functions.Function1;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = "WONCAN_DEVICE_MAIN";
@@ -157,12 +146,12 @@ public class MainActivity extends AppCompatActivity {
 //
 //            }
 //        });
-//        device.registerSatelliteListener(new SatelliteListener() {
-//            @Override
-//            public void onReceiveSatellite(List<SatelliteInfo> list) {
-//
-//            }
-//        });
+        device.registerSatelliteListener(new SatelliteListener() {
+            @Override
+            public void onReceiveSatellite(List<SatelliteInfo> list) {
+                Log.i(TAG, "onReceiveSatellite: "+list.size());
+            }
+        });
 
 //        device.closeRTCM();
 //        device.setLaserState(true);
